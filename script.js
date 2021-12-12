67ae9979f2c840a563a929d190c6121ee0baf0a1
If Pol <68429746+ifpolwebdev@users.noreply.github.com>
2021-12-12 21:43:27 +0200

    change placeholder text in html and add some logic in js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Duplicated previous project (palindromeChecker) to focus on JS
+// Duplicated previous project's (palindromeChecker) markup & styling to focus on JS
+const word = document.querySelector(".input-text");
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", countVowel);
+
+function countVowel() {
+  let vowelCount = 0;
+  let wordVal = word.value.toLowerCase();
+
+  for (let i = 0; i < wordVal.length; i++) {
+    let letter = wordVal[i];
+    if (letter.match(/([a,e,i,o,u])/)) {
+      vowelCount++;
+    }
+  }
+  result.innerHTML = `${word.value.toUpperCase()} has ${vowelCount} vowels`;
+}
